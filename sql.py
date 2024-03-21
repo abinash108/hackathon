5e842e34090b5d1662c91019b5fbd1ba6a1f8a3a
abinash108 <162575828+abinash108@users.noreply.github.com>
2024-03-21 20:23:48 +0530

    added features --csv outputs

--- sql.py
+++ sql.py
@@ -6,10 +6,12 @@ from SQLTOTEXT import Bot
 
 ## Configure Genai Key
 ## text to sql model
-model=Bot()
+model=Bot(mode=2)
+modes=['normal_mode','analytics_mode','english_qprompt']
 model.db="hr.db"
 app = hy.HydraApp(title='Secure Hydralit Data Explorer',favicon="🐙",hide_streamlit_markers=True,use_navbar=True, navbar_sticky=True)
 st.sidebar.success("Chat")
+
 @app.addapp()
 def AiSho():
 
@@ -17,7 +19,7 @@ def AiSho():
 
     ## Streamlit App
 
-
+    
     st.header("Your AI assistant")
 
     question=st.text_area(" ")
@@ -26,27 +28,8 @@ def AiSho():
 
     # if submit is clicked
     if submit:
-        english_res=model.eng_response(question)
-        st.header(english_res)
-
-@app.addapp()
-def Payroll ():
-    pass
-
-@app.addapp()
-def Leave():
-    pass
-
-def Calender ():
-    pass
-
-@app.addapp()
-def Training():
-    pass
-
-@app.addapp()
-def Analytics():
-    pass
+        display_text=model.eng_response(question)
+        st.header(display_text)
 
 app.run()
 
